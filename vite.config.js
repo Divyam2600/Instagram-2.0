893c4dae7a25823e6a7b0b4ea25007d61b2741e8
Divyam Agarwal <ag.divyam12@gmail.com>
2022-07-19 12:08:59 +0530

    base url added

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/repo/name', // base url
+  base: '/repo-name/', // base url
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
