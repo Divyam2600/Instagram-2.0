38006a90571f12b256f8a321f0768c32e9607ace
Divyam Agarwal <ag.divyam12@gmail.com>
2022-07-19 15:25:47 +0530

    updated to react 18 render

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./styles/index.css";
 import App from "./App";
 import FireBaseContext from "./context/firebase";
@@ -9,12 +9,13 @@ import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
 
 TimeAgo.addDefaultLocale(en);
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <FireBaseContext.Provider value={{ firebaseApp, value }}>
     <RecoilRoot>
       <App />
     </RecoilRoot>
   </FireBaseContext.Provider>,
-  document.getElementById("root")
 );
