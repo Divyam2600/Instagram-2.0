14cad90f3b15f428837447b111633068654b0b80
Divyam Agarwal <ag.divyam12@gmail.com>
2022-08-01 17:46:32 +0530

    💄 improved ui

--- src/components/Messages/ReceiverMessage.jsx
+++ src/components/Messages/ReceiverMessage.jsx
@@ -8,7 +8,8 @@ function ReceiverMessage({ message, image, username, sentAt }) {
       <p className="message-time ml-11">{moment(sentAt?.toDate()).format('LT')}</p>
       <div className="message-container">
         <img src={image} alt="" className="h-6 w-6 self-end rounded-full object-cover" />
-        <div className="message rounded-tl-none bg-purple-600 text-white">{message}</div>
+        <span className="absolute top-0 left-[26px] h-0 w-0 border-8 border-purple-600 border-x-transparent border-b-transparent" />
+        <div className="message bg-purple-600 text-white">{message}</div>
       </div>
     </>
   );
--- src/components/Messages/SenderMessage.jsx
+++ src/components/Messages/SenderMessage.jsx
@@ -5,9 +5,10 @@ import moment from 'moment';
 function SenderMessage({ message, sentAt }) {
   return (
     <>
-      <p className="message-time mr-2 self-end">{moment(sentAt?.toDate()).format('LT')}</p>
-      <div className="message-container justify-end self-end">
-        <div className="message rounded-tr-none bg-gray-200 bg-opacity-70">{message}</div>
+      <p className="message-time mr-3 self-end">{moment(sentAt?.toDate()).format('LT')}</p>
+      <div className="message-container justify-end self-end ">
+        <span className="absolute right-[2px] top-0 h-0 w-0 border-8 border-[#edeef1] border-x-transparent border-b-transparent" />
+        <div className="message bg-gray-200 bg-opacity-70">{message}</div>
       </div>
     </>
   );
