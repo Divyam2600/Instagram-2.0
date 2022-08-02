3355f0311f241de385e3e9eecd5742936bce5430
Divyam Agarwal <ag.divyam12@gmail.com>
2022-08-02 20:37:58 +0530

    💥  updated video ui

--- src/components/Messages/ReceiverMessage.jsx
+++ src/components/Messages/ReceiverMessage.jsx
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import { PauseIcon, PlayIcon } from '@heroicons/react/solid';
 
 function ReceiverMessage({ message, image, username, sentAt, isImage, isVideo }) {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+  function playVideo(event) {
+    event.preventDefault();
+    setPlaying(true);
+    videoRef.current.setAttribute('loop', true);
+    videoRef.current.play();
+  }
+  function pauseVideo(event) {
+    event.preventDefault();
+    setPlaying(false);
+    videoRef.current.pause();
+  }
   return (
     <>
       <p className="message-time ml-11">{moment(sentAt?.toDate()).format('LT')}</p>
       <div className="message-container">
         <img src={image} alt="" className="h-6 w-6 self-end rounded-full object-cover" />
-        <span className="absolute top-0 left-[26px] h-0 w-0 border-8 border-purple-600 border-x-transparent border-b-transparent" />
+        {!isImage && !isVideo && (
+          <span className="absolute top-0 left-[26px] h-0 w-0 border-8 border-purple-600 border-x-transparent border-b-transparent" />
+        )}
         <div
           className={`message bg-purple-600 text-white ${
-            (isImage || isVideo) && 'aspect-auto max-w-[50%] rounded-md px-1 lg:max-w-[35%]'
+            (isImage || isVideo) &&
+            'aspect-auto max-w-[50%] cursor-pointer bg-opacity-0 xxs:max-w-[60%] xs:max-w-[60%] lg:max-w-[35%]'
           }`}
         >
           {!isImage && !isVideo && message}
           {isImage && (
-            <img src={message} alt={`Sent By ${username}`} className="rounded-md object-cover" />
+            <img
+              src={message}
+              alt={`Sent By ${username}`}
+              className="-ml-2 rounded-2xl object-cover"
+            />
           )}
           {isVideo && (
-            <video
-              src={message}
-              autoPlay={true}
-              loop={true}
-              alt={`Sent By ${username}`}
-              className="rounded-md object-cover"
-            />
+            <>
+              <video
+                src={message}
+                preload={true}
+                alt={`Sent By ${username}`}
+                className="-ml-2 rounded-2xl object-cover hover:opacity-80"
+                ref={videoRef}
+              />
+              {!playing ? (
+                <PlayIcon
+                  className="absolute top-2 left-[50px] h-9 w-9 cursor-pointer stroke-2 text-white opacity-95 drop-shadow-lg"
+                  onClick={playVideo}
+                />
+              ) : (
+                <PauseIcon
+                  className="absolute top-2 left-[50px] h-9 w-9 cursor-pointer stroke-2 text-white opacity-95 drop-shadow-lg"
+                  onClick={pauseVideo}
+                />
+              )}
+            </>
           )}
         </div>
       </div>
--- src/components/Messages/SenderMessage.jsx
+++ src/components/Messages/SenderMessage.jsx
@@ -1,34 +1,60 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import { PauseIcon, PlayIcon } from '@heroicons/react/solid';
 
 function SenderMessage({ message, username, sentAt, isImage, isVideo }) {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+  function playVideo(event) {
+    event.preventDefault();
+    setPlaying(true);
+    videoRef.current.setAttribute('loop', true);
+    videoRef.current.play();
+  }
+  function pauseVideo(event) {
+    event.preventDefault();
+    setPlaying(false);
+    videoRef.current.pause();
+  }
   return (
     <>
       <p className="message-time mr-3 self-end">{moment(sentAt?.toDate()).format('LT')}</p>
       <div className="message-container justify-end self-end ">
-        <span className="absolute right-[2px] top-0 h-0 w-0 border-8 border-[#edeef1] border-x-transparent border-b-transparent" />
+        {!isImage && !isVideo && (
+          <span className="absolute right-[2px] top-0 h-0 w-0 border-8 border-[#edeef1] border-x-transparent border-b-transparent" />
+        )}
         <div
           className={`message bg-gray-200 bg-opacity-70 ${
             (isImage || isVideo) &&
-            'aspect-auto max-w-[50%] cursor-pointer rounded-md px-1 lg:max-w-[35%]'
+            'relative -mr-2 aspect-auto max-w-[50%] cursor-pointer bg-opacity-0 xxs:max-w-[60%] xs:max-w-[60%] lg:max-w-[35%]'
           }`}
         >
           {!isImage && !isVideo && message}
           {isImage && (
-            <img src={message} alt={`Sent By ${username}`} className="rounded-md object-cover" />
+            <img src={message} alt={`Sent By ${username}`} className="rounded-2xl object-cover" />
           )}
           {isVideo && message && (
-            <video
-              src={message}
-              controls
-              preload={true}
-              autoPlay={true}
-              loop={true}
-              alt={`Sent By ${username}`}
-              className="rounded-md object-cover"
-              disablePictureInPicture={true}
-            />
+            <>
+              <video
+                src={message}
+                preload={true}
+                alt={`Sent By ${username}`}
+                className="rounded-2xl object-cover hover:opacity-80"
+                ref={videoRef}
+              />
+              {!playing ? (
+                <PlayIcon
+                  className="absolute top-2 right-4 h-9 w-9 cursor-pointer stroke-2 text-white opacity-95 drop-shadow-lg"
+                  onClick={playVideo}
+                />
+              ) : (
+                <PauseIcon
+                  className="absolute top-2 right-4 h-9 w-9 cursor-pointer stroke-2 text-white opacity-95 drop-shadow-lg"
+                  onClick={pauseVideo}
+                />
+              )}
+            </>
           )}
         </div>
       </div>
