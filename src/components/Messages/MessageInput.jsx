0b561af5d8c1d9b0c6d7a31b870c259524f0c89d
Divyam Agarwal <ag.divyam12@gmail.com>
2022-08-05 22:01:00 +0530

    💫 ♻️ Refactored code

--- src/components/Messages/MessageInput.jsx
+++ src/components/Messages/MessageInput.jsx
@@ -37,7 +37,7 @@ function MessageInput({ messageId, messageRef }) {
     <div className="z-100 sticky my-2 mx-auto flex w-[95%] items-center space-x-1 self-end rounded-3xl border p-1">
       <textarea
         placeholder="Enter your message ..."
-        className="m-0 h-5 max-h-24 max-w-full flex-1 xxs:placeholder:text-[12px] resize-none overflow-auto border-r bg-transparent px-4 text-sm outline-none scrollbar-hide"
+        className="m-0 h-5 max-h-24 max-w-full flex-1 resize-none overflow-auto border-r bg-transparent px-4 text-sm outline-none scrollbar-hide xxs:placeholder:text-[12px]"
         onChange={(event) => setMessage(event.target.value)}
         autoComplete="off"
         value={message}
