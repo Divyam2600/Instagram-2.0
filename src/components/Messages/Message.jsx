90f2708ff6cf74fd97918ba872507abc23ca4a6e
Divyam Agarwal <ag.divyam12@gmail.com>
2022-08-09 16:12:36 +0530

    ✨ added group by date

--- src/components/Messages/Message.jsx
+++ src/components/Messages/Message.jsx
@@ -6,6 +6,7 @@ import { firebaseApp } from '../../lib/firebase';
 import { getChats } from '../../services/firebase';
 import useUser from '../../hooks/use-user';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 function Message({ messageId }) {
   const [chats, setChats] = useState([]);
@@ -21,35 +22,62 @@ function Message({ messageId }) {
     }
     showChats();
   }, [db, messageId]);
+  const isDifferentDay = (messageIndex) => {
+    if (messageIndex === 0) return true;
+    const day1 = chats[messageIndex - 1]?.data().sentAt?.toDate();
+    const day2 = chats[messageIndex]?.data().sentAt?.toDate();
+    return (
+      day1.getFullYear() != day2?.getFullYear() ||
+      day1.getMonth() != day2?.getMonth() ||
+      day1.getDate() != day2?.getDate()
+    );
+  };
   return (
     chats.length > 0 &&
-    chats?.map((chat) =>
-      chat.data().sender === username ? (
-        <SenderMessage
-          key={chat.id}
-          message={chat.data().message}
-          image={chat.data().image}
-          username={chat.data().sender}
-          sentAt={chat.data().sentAt}
-          isImage={chat.data().isImage}
-          isVideo={chat.data().isVideo}
-          isAudio={chat.data().isAudio}
-          audioId={chat.data().audioId}
-        />
-      ) : (
-        <ReceiverMessage
-          key={chat.id}
-          message={chat.data().message}
-          image={chat.data().image}
-          username={chat.data().sender}
-          sentAt={chat.data().sentAt}
-          isImage={chat.data().isImage}
-          isVideo={chat.data().isVideo}
-          isAudio={chat.data().isAudio}
-          audioId={chat.data().audioId}
-        />
-      )
-    )
+    chats?.map((chat, i) => (
+      <div key={i} className="">
+        {isDifferentDay(i) && (
+          <div
+            className="sticky top-0 my-2 mx-auto max-w-fit items-start rounded-lg bg-gray-300 py-1 px-4 text-center text-sm text-black"
+            key={i}
+          >
+            {moment(chats[i]?.data().sentAt?.toDate()).calendar({
+              sameDay: '[Today]',
+              nextDay: '[Tomorrow]',
+              nextWeek: 'MMMM D, YYYY',
+              lastDay: '[Yesterday]',
+              lastWeek: 'MMMM D, YYYY',
+              sameElse: 'MMMM D, YYYY'
+            })}
+          </div>
+        )}
+        {chat.data().sender === username ? (
+          <SenderMessage
+            key={chat.id}
+            message={chat.data().message}
+            image={chat.data().image}
+            username={chat.data().sender}
+            sentAt={chat.data().sentAt}
+            isImage={chat.data().isImage}
+            isVideo={chat.data().isVideo}
+            isAudio={chat.data().isAudio}
+            audioId={chat.data().audioId}
+          />
+        ) : (
+          <ReceiverMessage
+            key={chat.id}
+            message={chat.data().message}
+            image={chat.data().image}
+            username={chat.data().sender}
+            sentAt={chat.data().sentAt}
+            isImage={chat.data().isImage}
+            isVideo={chat.data().isVideo}
+            isAudio={chat.data().isAudio}
+            audioId={chat.data().audioId}
+          />
+        )}
+      </div>
+    ))
   );
 }
 
